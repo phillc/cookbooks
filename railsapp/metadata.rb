ef24557b159425267df0b60917baa1e2adbe1b0e
phillc <pccampbell@patrickdavis.com>
2009-12-21 15:08:36 -0500

    Fixed some metadata

--- railsapp/metadata.rb
+++ railsapp/metadata.rb
@@ -1,6 +1,6 @@
 maintainer        "Phillip Campbell"
-maintainer_email  "pccampbell@patrickdavis.com"
-description       "Sets up an app server for the Davis website."
+maintainer_email  "spyyderz@gmail.com"
+description       "Sets up an app server for the Rails."
 
 %w{ passenger_apache2::mod_rails rails git mysql::client }.each do |cb|
   depends cb
